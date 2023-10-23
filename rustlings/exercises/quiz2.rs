4c7bbc60c87a14d0ef07a73afada4194013599db
Daniele Trapani <daniele.trapani96@gmail.com>
2023-10-23 15:57:15 +0200

    hashmaps exercises and second quiz

--- rustlings/exercises/quiz2.rs
+++ rustlings/exercises/quiz2.rs
@@ -32,20 +32,35 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(uppercase(string)),
+                Command::Trim => output.push(trim(string)),
+                Command::Append(amount) => output.push(append(string, *amount)),
+            }
         }
         output
+    }
+
+    fn uppercase(string: &str) -> String {
+        string.to_uppercase()
+    }
+
+    fn trim(string: &str) -> String {
+        string.trim().to_string()
+    }
+
+    fn append(string: &str, amount: usize) -> String {
+        let addition = "bar".to_string().repeat(amount);
+        string.to_string() + &addition
     }
 }
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
