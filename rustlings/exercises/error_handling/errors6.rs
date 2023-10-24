ce38e853894afd15e80adfdc2d0b4cd6bed353c4
Daniele Trapani <daniele.trapani96@gmail.com>
2023-10-24 20:02:52 +0200

    options and errors

--- rustlings/exercises/error_handling/errors6.rs
+++ rustlings/exercises/error_handling/errors6.rs
@@ -24,14 +24,22 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
-    // fn from_parseint...
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x = s.parse().unwrap();
+    match x {
+        Ok(x) => PositiveNonzeroInteger::new(x),
+        Err(err) => match err {
+            ParseIntError => ParsePosNonzeroError::from_parseint(err),
+            CreationError => ParsePosNonzeroError::from_creation(err),
+        },
+    }
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
